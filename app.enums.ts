cd15551d28edfdd399ee2b7b048e0d1b049734a8
Ali Jafari <ali1dc@gmail.com>
2022-04-28 10:39:21 -0400

    using enum

---
+++ app.enums.ts
@@ -0,0 +1,6 @@
+export enum API_VERSION {
+  VERSION_1 = '1',
+  VERSION_2 = '2',
+  CURRENT = VERSION_1,
+  LATEST = VERSION_2,
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,18 +2,19 @@ import { Controller, Get, Version, VERSION_NEUTRAL } from '@nestjs/common';
 import { UserV1Dto } from './dto/user.v1.dto';
 import { UserV2Dto } from './dto/user.v2.dto';
 import { UserService } from './user.service';
+import { API_VERSION } from 'app.enums';
 
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
 
-  @Version(['1', VERSION_NEUTRAL])
+  @Version([API_VERSION.VERSION_1, VERSION_NEUTRAL])
   @Get()
   findAllV1(): UserV1Dto[] {
     return this.userService.findAllV1()
   }
 
-  @Version('2')
+  @Version(API_VERSION.VERSION_2)
   @Get()
   findAllV2(): UserV2Dto[] {
     return this.userService.findAllV2()
